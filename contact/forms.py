273aaf1ed5f25d1a4f837c7b2467b23d2c917038
unknown <ermisselimcan@gmail.com>
2019-07-18 16:12:36 +0300

    first commit

---
+++ contact/forms.py
@@ -0,0 +1,30 @@
+
+from django import forms
+from .models import Contact
+
+class ContactForm(forms.ModelForm):
+    class Meta:
+        model = Contact
+        fields = ['name', 'email', 'phone', 'message']
+        widgets = {
+            'name': forms.TextInput(
+                attrs={
+                    'placeholder': 'Name'
+                }
+            ),
+            'email': forms.EmailInput(
+                attrs={
+                    'placeholder': 'Email'
+                }
+            ),
+            'phone': forms.TextInput(
+                attrs={
+                    'placeholder': 'Phone Number'
+                }
+            ),
+            'message': forms.Textarea(
+                attrs={
+                    'placeholder': 'Message'
+                }
+            ),
+        }
